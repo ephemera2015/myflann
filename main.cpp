0352888328465167272622d2a3c990108db78901
ephemera2015 <ephemera_2015@sina.com>
2016-07-29 22:36:27 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,55 +13,7 @@ const string test_float_flann_query = "data//sift_query.fvecs";
 const string test_float_flann_truth = "data//sift_groundtruth.ivecs";
 
 
-void test_lsh_precise_time(ostream& out, vector<int>& table_numbers, vector<int>&key_size,vector<float>& scales)
-{
-	DataReader<float> floatReader;
-	DataReader<int>   intReader;
-	int base_cnt;
-	int query_cnt;
-	int truth_cnt;
-	Matrix<float> base = floatReader.read(test_float_flann_base, base_cnt);
-	Matrix<float> query = floatReader.read(test_float_flann_query, query_cnt);
-	Matrix<int>   truth = intReader.read(test_float_flann_truth, truth_cnt);
-	out << "********************************LSH测试*********************************" << endl;
-	out << endl;
-	out << setiosflags(ios::left) << setw(10) << "table_num"
-		<< setiosflags(ios::left) << setw(10) << "key_size"
-		<< setiosflags(ios::left) << setw(10) << "scales"
-		<< setiosflags(ios::left) << setw(8) << "准确率"
-		<< setiosflags(ios::left) << setw(14) << "查询用时ms"
-		<< setiosflags(ios::left) << setw(14) << "索引用时ms" << endl;
-	int i, j, k;
-	for (i = 0; i < table_numbers.size(); ++i)
-	{
-		for (j = 0; j < key_size.size(); ++j)
-		{
 
-			LshIndexParams p(table_numbers[i], key_size[j]);
-			Index<L2<float>> index(base, p);
-			clock_t search_end, search_begin, build_begin, build_end;
-			build_begin = clock();
-			index.buildIndex();
-			build_end = clock();
-			for (k = 0; k < scales.size();++k)
-			{
-				SearchParam sp(scales[k]);
-				vector<vector<int>>indices;
-				vector<vector<float>> dists;
-				search_begin = clock();
-				index.knnSearch(query, indices, dists, 1, sp);
-				search_end = clock();
-				out << setiosflags(ios::left) << setw(10) << table_numbers[i];
-				out << setiosflags(ios::left) << setw(10) << key_size[j];
-				out << setiosflags(ios::left) << setw(10) << scales[k];
-				out << setiosflags(ios::left) << setw(8) << setiosflags(ios::fixed) << setprecision(3) << precise_rate(truth, indices, 100);
-				out << setiosflags(ios::left) << setw(14) << setiosflags(ios::fixed) << setprecision(2) << (search_end - search_begin);
-				out << setiosflags(ios::left) << setw(14) << setiosflags(ios::fixed) << setprecision(2) << (build_end - build_begin) << endl;
-			}
-			
-		}
-	}
-}
 void test_dsh_precise_time(ostream& out, vector<int>& table_numbers, vector<int>&key_size,vector<float>& scales)
 {
 	DataReader<float> floatReader;
@@ -121,8 +73,8 @@ int main()
 	    //ofstream fout("result.txt");//将在工程目录下生成result.txt文件
 		//cout.rdbuf(fout.rdbuf());
 		vector<int> table_numbers={80};
-		vector<int> key_sizes = {15,23,30,38,45,50,56,64};
-		vector<float> scales = { 1.0,5.0,10.0,20.0,30.0,50.0};
+		vector<int> key_sizes = {15,23,30,38,45,50,56};
+		vector<float> scales = { 1.0,5.0,10.0,20.0,30.0};
 		test_dsh_precise_time(cout, table_numbers,key_sizes,scales);//测试dsh有效率
 		cout.rdbuf(old);
 		cout << "测试已结束" << endl;
